d6ce0c10d3aea8ae196a2ba05785432372f7c885
Khush Bakht Naveed <khush.naveed@dci-student.org>
2024-10-28 21:44:14 +0100

    Clean code

--- Hangman Game/main.js
+++ Hangman Game/main.js
@@ -1,46 +1,116 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-import { readFileAsync, centerBoxedMessage } from "./functions.js";
-import { msg, msgOption } from "./variables.js";
+import {
+  readFileAsync,
+  centerBoxedMessage,
+  getRandomWord,
+  gameWordPuzzle,
+} from "./functions.js";
+import { boxedMsg, boxedMsgGameOver, boxedMsgCongrats } from "./variables.js";
+
+console.clear();
 
 //print a welcome msg
-console.log(centerBoxedMessage(chalk.bgRedBright.bold(msg)));
+console.log(centerBoxedMessage(chalk.bgRed.bold(boxedMsg)));
 
-//do-while loop declaration
+//do-while for repeatedly asking user choice to select the game from different categories
 do {
-
-  //getting user option
+  //getting user's input from selection of list
   const userAnswer = await inquirer.prompt({
     type: "list",
     name: "selectedOption",
     message: "Select your option >>",
     choices: ["Animals", "Fruits", "Furniture", "Quit"],
   });
-  
-  //print user option
-  console.log(userAnswer);
 
-  //switch case for implementing user#s 
+  //switch case for implementing users selection from list
   switch (userAnswer.selectedOption) {
     case "Animals":
-      console.log(msgOption);
+      // calling readFileAsync function to read the string data from the file
       const animalsData = await readFileAsync("./Sample-words/animals.txt");
-      console.log(animalsData);
+
+      // calling getRandomWord function to get the random word(array) from the data for the game
+      const randomWord = getRandomWord(animalsData);
+      console.log("random word", randomWord);
+
+      // calling gameWorldPuzzle function to map a guess puzzle '_'
+      const gameWordMap = gameWordPuzzle(randomWord);
+
+      // setting couter as maximum number of user's guesses
+      let counter = gameWordMap.length + 5;
+      console.log("Length of the word: ", gameWordMap.length); // sending hint
+
+      // do while loop untill the user run out of the maximum guesses
+      do {
+        console.log("Remianing Chances: ", counter);
+        // getting alphabets step by step from the user
+        const guessAlphabet = await inquirer.prompt({
+          type: "input",
+          name: "Word",
+          message: "Guess the words step by step >>",
+          validate: (input) => {
+            if (!/^[a-zA-Z]$/.test(input)) {
+              return "Please enter only a single letter.";
+            }
+            return true;
+          },
+        });
+
+        if (randomWord.includes(guessAlphabet.Word)) {
+          // Check if the letter is already in gameWord to avoid repeated guesses
+          if (gameWordMap.includes(guessAlphabet.Word)) {
+            console.log(
+              "You've already guessed that letter! Try a different one."
+            );
+          } else {
+            // Update all occurrences of the guessed letter in gameWord
+            randomWord.forEach((letter, index) => {
+              if (letter === guessAlphabet.Word) {
+                gameWordMap[index] = guessAlphabet.Word;
+              }
+            });
+            console.log("Correct Guess ✅");
+            console.log(gameWordMap);
+          }
+        } else {
+          console.log("Incorrect Guess ❌");
+        }
+
+        if (gameWordMap.join("") === randomWord.join("")) {
+          console.log(centerBoxedMessage(chalk.bgGreen.black.bold("Correct Word: ", randomWord.join(""))));
+          console.log(centerBoxedMessage(chalk.bgGreen.black.bold(boxedMsgCongrats)));
+          break;
+        }
+
+        counter -= 1;
+        
+      } while (counter > 0);
+
+      if (counter === 0) {
+        console.log(centerBoxedMessage(chalk.bgRed.bold(boxedMsgGameOver)));
+      }
+
       console.log("________________________");
       break;
 
     case "Fruits":
-      console.log(msgOption);
       const fruitsData = await readFileAsync("./Sample-words/fruits.txt");
-      console.log(fruitsData);
+      const randomWordFruits = getRandomWord(fruitsData);
+      console.log("random word", randomWord);
+      const gameWordMapFruits = gameWordPuzzle(randomWordFruits);
+      let counterFruits = gameWordMap.length + 5;
+      console.log("Length of the word: ", gameWordMapFruits.length);
       console.log("________________________");
       break;
 
     case "Furniture":
-      console.log(msgOption);
       const furnitureData = await readFileAsync("./Sample-words/furniture.txt");
-      console.log(furnitureData);
+      const randomWordFurniture = getRandomWord(furnitureData);
+      console.log("random word", randomWord);
+      const gameWordMapFurniture = gameWordPuzzle(randomWordFurniture);
+      let counterFurniture = gameWordMap.length + 5;
+      console.log("Length of the word: ", gameWordMapFurniture.length);
       console.log("________________________");
       break;
 
--- Hangman Game/variables.js
+++ Hangman Game/variables.js
@@ -1,2 +1,21 @@
-export const msg = "Welcome to the Hangman Game! 🎉🔠💀";
-export const msgOption = "Folowing are the words:";
+import boxen from "boxen";
+
+const welcomeMsg = "Welcome to the Hangman Game! 🎉🔠💀";
+const gameOverMessage =
+  "👾 😵 🎮 💀 🕹️ ☠️ ❌ 🎮. 🎲 😈... GAME OVER...👾 😵 🎮 💀 🕹️ ☠️ ❌ 🎮. 🎲 😈";
+const congratsMsg =
+  "🥳🎉👏🤝👍🌷 Congratulations! You've guessed the word correctly! 🥳🎉👏🤝👍🌷 ";
+
+const boxedMsg = boxen(welcomeMsg, {
+  padding: 1,
+});
+
+const boxedMsgGameOver = boxen(gameOverMessage, {
+  padding: 0.5,
+});
+
+const boxedMsgCongrats = boxen(congratsMsg, {
+  padding: 0.5,
+});
+
+export { boxedMsg, boxedMsgGameOver, boxedMsgCongrats };
